b10a1fa4fd3f36e31c5107e7f2f1f8ddadbe24ae
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-05-18 17:17:46 +0800

    Finished lesson 10.

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,17 @@
 import React from 'react';
 import './App.css';
+import {Technologies} from './Technologies';
+import {Header} from './Header';
 
 const App = () => {
     return (
         <div className="App">
             <Header/>
             <Technologies/>
+            <F/>
         </div>
     );
 };
-const Header = () => {
-    return (<div>
-        <a href="">Home </a>
-        <a href="">News Feed </a>
-        <a href="">Messages </a>
-    </div>);
-};
-const Technologies = () => {
-    return (
-        <ul>
-            <li>SSC</li>
-            <li>HTML</li>
-            <li>JS</li>
-            <li>React</li>
-        </ul>
-    );
-};
+
 
 export default App;
