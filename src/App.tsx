9207a544046e6969c077d1d95dfdc0cb13fd5a19
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-05-18 16:52:25 +0800

    Finished lesson 7. Create new components:Header,Technologies

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className="App">
+            <Header/>
+            <Technologies/>
+        </div>
+    );
+};
+const Header = () => {
+    return (<div>
+        <a href="">Home </a>
+        <a href="">News Feed </a>
+        <a href="">Messages </a>
+    </div>);
+};
+const Technologies = () => {
+    return (
+        <ul>
+            <li>SSC</li>
+            <li>HTML</li>
+            <li>JS</li>
+            <li>React</li>
+        </ul>
+    );
+};
 
 export default App;
