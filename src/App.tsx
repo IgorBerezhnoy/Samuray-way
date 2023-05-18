dee60e0e5dc6b5679c007fdef8ca53246443cdc8
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-05-18 19:02:14 +0800

    Finished lesson 11.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import './App.css';
-import {Technologies} from './Technologies';
-import {Header} from './Header';
+
 
 const App = () => {
     return (
-        <div className="App">
-            <Header/>
-            <Technologies/>
-            <F/>
+        <div className="app-wrapper">
+            <header className={'header'}><img src={'https://partsouq.com/images/logos-transparent/jeep.png'}/></header>
+            <nav className={'nav'}>
+                <div><a href={''}>Profile</a></div>
+                <div><a href={''}>Messages</a></div>
+                <div><a href={''}>News</a></div>
+                <div><a href={''}>Settings</a></div>
+            </nav>
+            <div className={'content'}>
+                <div><img src={'https://img5.goodfon.ru/original/800x480/0/67/kanada-ozero-bow-lake-1.jpg'}/></div>
+                <div><img src={'https://wa-groups.ru/img/user5.png'} width={'200px'}/> avatar + description</div>
+                <div>My post
+                    <div>New post
+                        <div>post 1</div>
+                        <div>post 1</div>
+                        <div>post 1</div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
