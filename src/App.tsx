9604b375c9cf0c9220131664f2df5428eeb14525
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-09-12 03:25:15 +0800

    finished lesson #75 and task

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import {NavbarContainer} from './components/NavBar/NavbarContainer';
 import UsersContainer from './components/users/UsersContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
-import {Login} from './components/Login/Login';
+import {Login, LoginContainer} from './components/Login/Login';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
 
 
@@ -37,7 +37,7 @@ const App: React.FC = () => {
                 <Route path={'/news'} render={() => <News/>}/>
                 <Route path={'/music'} render={() => <Music/>}/>
                 <Route path={'/settings'} render={( ) => <Settings/>}/>
-                <Route path={'/login'} render={() => <Login/>}/>
+                <Route path={'/login'} render={() => <LoginContainer/>}/>
                 {/*<Music/>*/}
             </div>
 
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import {Field, InjectedFormProps, reduxForm} from 'redux-form';
-import {useDispatch} from 'react-redux';
+import {connect} from 'react-redux';
 import {loginDateTC} from '../../Redux/auth-reducer';
 import {formDateType} from '../../api/Api';
+import {AppRootStateType} from '../../Redux/redux-store';
+import {Redirect} from 'react-router-dom';
 
-export const Login = () => {
-    let
-let dispatch=useDispatch()
-    const onSubmit=(formDate:any)=>{
-let data:formDateType={email:formDate.login as string, password:formDate.password as string, rememberMe:true}
-        dispatch(loginDateTC(data))
+type PropsType = {
+    isAuth: boolean,
+    loginDateTC: (data: formDateType) => void
+}
+export const Login = (props: PropsType) => {
+    const onSubmit = (formDate: any) => {
+        let data: formDateType = {
+            email: formDate.login as string,
+            password: formDate.password as string,
+            rememberMe: formDate.rememberMe
+        };
+        props.loginDateTC(data);
+    };
+    if (props.isAuth) {
+        return <Redirect to={'profile'}/>;
     }
     return (
         <div><h1>
@@ -19,12 +30,20 @@ let data:formDateType={email:formDate.login as string, password:formDate.passwor
         </div>
     );
 };
+let mapStateToProps = (state: AppRootStateType) => {
+    return {
+        isAuth: state.authMe.isAuth
+    };
+};
 
-let LoginForm:React.FC<InjectedFormProps> = (props) => {
+export let LoginContainer = connect(mapStateToProps, {
+    loginDateTC
+})(Login);
+let LoginForm: React.FC<InjectedFormProps> = (props) => {
     return (<form onSubmit={props.handleSubmit}>
-        <div><Field component={'input'} name={"login"} placeholder={'Login'}/></div>
-        <div><Field component={'input'} name={"password"} type={'password'} placeholder={'Password'}/></div>
-        <div><Field component={'input'} name={"RememberMe"} type={'checkbox'}/>Remember Me</div>
+        <div><Field component={'input'} name={'login'} placeholder={'Login'}/></div>
+        <div><Field component={'input'} name={'password'} type={'password'} placeholder={'Password'}/></div>
+        <div><Field component={'input'} name={'rememberMe'} type={'checkbox'}/>Remember Me</div>
         <div>
             <button>Login</button>
         </div>
