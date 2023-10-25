4b52e7695d00da300048bf373a0ec16d429bf252
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-10-26 00:14:41 +0800

    change reducer/added css change dialogs #1

--- src/Redux/diologs-reducer.ts
+++ src/Redux/diologs-reducer.ts
@@ -1,59 +1,122 @@
-
 let initialState = {
-    dialogs: [
-        {id: '1', name: 'Dimych', srs: `${process.env.PUBLIC_URL}/img/user5.png`},
-        {id: '2', name: 'Andrew', srs: `${process.env.PUBLIC_URL}/img/user7.png`},
-        {id: '3', name: 'Sveta', srs: `${process.env.PUBLIC_URL}/img/1761894.png`},
-        {id: '4', name: 'Sasha', srs: `${process.env.PUBLIC_URL}/img/Sveta.png`},
-        {id: '5', name: 'Valera', srs: `${process.env.PUBLIC_URL}/img/1067538.png`},
-        {
-            id: '6',
-            name: 'Viktor',
-            srs: `${process.env.PUBLIC_URL}/img/png-transparent-account-avatar-profile-user-avatars-icon.png`
-        },
-    ],
-    messages: [
-        {id: 1, message: 'Hi'},
+  dialogs: [
+    {
+      id: '1', name: 'Dimych', srs: `${process.env.PUBLIC_URL}/img/user5.png`,
+      outgoingMessages: [
+        {id: 1, message: 'Hello'},
         {id: 2, message: 'How are you?'},
-        {id: 3, message: 'What are you doing'},
-        {id: 3, message: 'Yo'}
-    ],
+        {id: 3, message: 'I am fine. What are you doing'},
+      ],
+      incomingMessages: [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'Good, and you?'},
+      ]
+    },
+    {
+      id: '2', name: 'Andrew', srs: `${process.env.PUBLIC_URL}/img/user7.png`,
+      outgoingMessages: [
+        {id: 1, message: 'Hello'},
+        {id: 2, message: 'How are you?'},
+        {id: 3, message: 'I am fine. What are you doing'},
+      ],
+      incomingMessages: [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'Good, and you?'},]
+    },
+    {
+      id: '3', name: 'Sveta', srs: `${process.env.PUBLIC_URL}/img/1761894.png`,
+      outgoingMessages: [
+        {id: 1, message: 'Hello'},
+        {id: 2, message: 'How are you?'},
+        {id: 3, message: 'I am fine. What are you doing'},
+      ],
+      incomingMessages: [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'Good, and you?'},
+      ]
+    },
+    {
+      id: '4', name: 'Sasha', srs: `${process.env.PUBLIC_URL}/img/Sveta.png`,
+      outgoingMessages: [
+        {id: 1, message: 'Hello'},
+        {id: 2, message: 'How are you?'},
+        {id: 3, message: 'I am fine. What are you doing'},
+      ],
+      incomingMessages: [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'Good, and you?'},
+      ]
+    },
+    {
+      id: '5', name: 'Valera', srs: `${process.env.PUBLIC_URL}/img/1067538.png`,
+      outgoingMessages: [
+        {id: 1, message: 'Hello'},
+        {id: 2, message: 'How are you?'},
+        {id: 3, message: 'I am fine. What are you doing'},
+      ],
+      incomingMessages: [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'Good, and you?'},
+      ]
+    },
+    {
+      id: '6',
+      name: 'Viktor',
+      srs: `${process.env.PUBLIC_URL}/img/png-transparent-account-avatar-profile-user-avatars-icon.png`,
+      outgoingMessages: [
+        {id: 1, message: 'Hello'},
+        {id: 2, message: 'How are you?'},
+        {id: 3, message: 'I am fine. What are you doing'},
+      ],
+      incomingMessages: [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'Good, and you?'},
+      ]
+
+
+    },
+  ],
 };
+
 export const dialogsReducer = (state: StateType = initialState, action: DialogsActionType): StateType => {
-    switch (action.type) {
-        case 'dialogs/ADD-MESSAGE':
-            let newMessage: MassageType = {
-                id: state.messages.length,
-                message: action.message
-            };
+  switch (action.type) {
+    case 'dialogs/ADD-MESSAGE':
+      let newMessage: MassageType = {
+        id: state.dialogs.find(el => el.id === action.userId)!.incomingMessages.length,
+        message: action.message
+      };
+      return {...state,
+        dialogs: state.dialogs.map(el => el.id === action.userId ? {
+          ...el,
+          incomingMessages: [...el.incomingMessages, newMessage]
+        } : el)
+      };
 
-            return {...state, messages: [...state.messages, newMessage]};
-
-        default:
-            return state;
-    }
+    default:
+      return state;
+  }
 
 
 };
-export const addMessage = (message: string): AddMessageTypeAT => ({type: "dialogs/ADD-MESSAGE", message});
+export const addMessage = (message: string, userId: string) => ({type: 'dialogs/ADD-MESSAGE', message, userId});
 
 
 export type DialogType = {
-    id: string
-    name: string
-    srs: string
+  id: string
+  name: string
+  srs: string
 }
 export type DialogsType = DialogType[]
 
 export type MassageType = {
-    id: number
-    message: string
+  id: number
+  message: string
 
 }
 export type MessagesType = MassageType[]
 
-export type AddMessageTypeAT = { type: 'dialogs/ADD-MESSAGE', message: string }
+export type AddMessageTypeAT = ReturnType<typeof addMessage>
 export type  updateNewMessageTextTypeAT = { type: 'dialogs/UPDATE-NEW-MESSAGE-TEXT', newText: string }
 
-type StateType = { dialogs: DialogsType, messages: MessagesType }
+type StateType = typeof initialState
 export type DialogsActionType = AddMessageTypeAT
--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,75 +1,74 @@
-import React, {KeyboardEvent} from 'react';
+import React from 'react';
 import s from './Dialogs.module.css';
 import {DialogItem} from './DialogItem/DialogItem';
 import {Message} from './Message/Message';
 import {Field, InjectedFormProps, reduxForm} from 'redux-form';
 import {DialogsType, MessagesType} from '../../Redux/diologs-reducer';
-import {maxLength150, maxLength30, requiredField} from '../../utils/validators/validators';
-import {Input} from '../common/FormsControls/FormsControls';
+import {maxLength150, requiredField} from '../../utils/validators/validators';
+import {Input} from '../common';
 
 type PropsType = {
 
-    dialogsPage: {
-        dialogs: DialogsType, messages: MessagesType, newMessageText: string
-    }
-    addMessage: (message: string) => void
+  dialogsPage: {
+    dialogs: DialogsType, messages: MessagesType, newMessageText: string
+  }
+  addMessage: (message: string) => void
 };
 
 export const Dialogs: React.FC<PropsType> = (props) => {
 
-    let dialogsItems = props.dialogsPage.dialogs.map(el =>
-        <DialogItem key={el.id} name={el.name} id={el.id} srs={el.srs}/>);
+  let dialogsItems = props.dialogsPage.dialogs.map(el =>
+    <DialogItem key={el.id} name={el.name} id={el.id} srs={el.srs}/>);
 
-    let messagesItems = props.dialogsPage.messages.map(el => <Message key={el.id} message={el.message}/>);
+  let messagesItems = props.dialogsPage.messages.map(el => <Message key={el.id} message={el.message}/>);
 
 
-    const onSubmit = (formDate: any) => {
-        console.log(formDate);
-        props.addMessage(formDate.newMessageBody);
-        formDate.newMessageBody = '';
-    };
+  const onSubmit = (formDate: any) => {
+    console.log(formDate);
+    props.addMessage(formDate.newMessageBody);
+    formDate.newMessageBody = '';
+  };
 
-    return (
-        <>
-            <div className={s.dialogs}>
-                <div className={s.dialogsWrapper}>
-                    <div className={s.dialogsItems}>
-                        {dialogsItems}
-                    </div>
-                    <div className={s.messages}>
-                        {messagesItems}
-                    </div>
+  return (
+    <>
+      <div className={s.dialogs}>
+        <div className={s.dialogsWrapper}>
+          <div className={s.dialogsItems}>
+            {dialogsItems}
+          </div>
+          <div className={s.messages}>
+            {messagesItems}
+          </div>
+        </div>
 
-                </div>
-
-            </div>
-            <div className={s.addForm}>
-                <AddMessageReduxForm onSubmit={onSubmit}/>
-                {/*<textarea value={props.dialogsPage.newMessageText} onChange={onMessageChange}*/}
-                {/*          style={{width: '827px', height: '58px'}} ref={newMessageElement}*/}
-                {/*          onKeyPress={OnClickEnter} placeholder={'Enter your message'}> </textarea>*/}
-                {/*<button onClick={addMessage}>Send</button>*/}
-            </div>
-        </>
-    );
+      </div>
+      <div className={s.addForm}>
+        <AddMessageReduxForm onSubmit={onSubmit}/>
+        {/*<textarea value={props.dialogsPage.newMessageText} onChange={onMessageChange}*/}
+        {/*          style={{width: '827px', height: '58px'}} ref={newMessageElement}*/}
+        {/*          onKeyPress={OnClickEnter} placeholder={'Enter your message'}> </textarea>*/}
+        {/*<button onClick={addMessage}>Send</button>*/}
+      </div>
+    </>
+  );
 };
 
 
 const AddMessageForm: React.FC<InjectedFormProps> = (props) => {
 
 
-    return (
-        <form onSubmit={props.handleSubmit}>
-            <div className={s.addForm}>
-                <Field component={Input} name={'newMessageBody'}
-                       placeholder={'Enter your message'} style={{width: '827px', height: '58px'}}
-                       validate={[requiredField, maxLength150]}/>
-                <button>Send</button>
-            </div>
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <div className={s.addForm}>
+        <Field component={Input} name={'newMessageBody'}
+               placeholder={'Enter your message'} style={{width: '827px', height: '58px'}}
+               validate={[requiredField, maxLength150]}/>
+        <button>Send</button>
+      </div>
 
 
-        </form>
-    );
+    </form>
+  );
 };
 
 const AddMessageReduxForm = reduxForm({form: 'dialogAddMessageForm'})(AddMessageForm);
